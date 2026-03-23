55e13fa6509b0a689cdf0e6ccf54c415648780e9
20253ds111-coder <20253ds111@utez.edu.mx>
2026-03-23 08:49:36 -0600

    terminé

--- src/main/java/com/consultorio/utez2dpacientesjavafxequipo08/HelloController.java
+++ src/main/java/com/consultorio/utez2dpacientesjavafxequipo08/HelloController.java
@@ -1,14 +1,155 @@
-package com.consultorio.utez2dpacientesjavafxequipo08;
+package com.example.demolistview.controllers;
 
+import com.example.demolistview.services.PersonServices;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+import java.io.IOException;
+import java.util.List;
+
+public class AppController {
     @FXML
-    private Label welcomeText;
+    private ListView<String> listView;
+    @FXML
+    private Label lblMsg;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtEdad;
+    @FXML
+    private TextField txtBusqueda;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private final ObservableList<String> data = FXCollections.observableArrayList();
+
+    private PersonServices service = new PersonServices();
+
+    @FXML
+    public void initialize(){ //Se ejecuta al inicio en cuanto se cargue el controller
+        //Inicializar ListView
+        loadFromFile();
+        txtBusqueda.textProperty().addListener(
+                (obs,old,newValue)->{
+                    filterList(newValue);
+                }
+        );
+
+        listView.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) ->{
+                    loadDataToForm(newValue);
+                }
+        );
+        listView.setItems(data);
+    }
+
+    private void filterList(String dato) {
+        if (dato == null || dato.isEmpty()){
+            initialize();
+            return;
+        }
+        ObservableList<String> finalList = FXCollections.observableArrayList();
+        String lower = dato.toLowerCase();
+        for (String item : data){
+            String[] parts = item.split("-",-1);
+            String email = parts[1].trim().toLowerCase();
+            if (email.contains(lower)){
+                finalList.add(item);
+            }
+        }
+        listView.setItems(finalList);
+    }
+
+
+    @FXML
+    public void onAddPerson() throws IOException {
+        try {
+            String name = txtName.getText();
+            String email = txtEmail.getText();
+            String edad = txtEdad.getText();
+            service.addPerson(name,email,edad);
+            lblMsg.setText("Persona agregada con exito ");
+            lblMsg.setStyle("-fx-text-fill: green");
+            txtName.clear();
+            txtEmail.clear();
+            txtEdad.clear();
+            loadFromFile();
+
+        }catch (IOException e){
+            lblMsg.setText("Hubo un error ");
+            lblMsg.setStyle("-fx-text-fill: red");
+
+        }catch (IllegalArgumentException ex){
+            lblMsg.setText(ex.getMessage());
+            lblMsg.setStyle("-fx-text-fill: red");
+        }
+    }
+
+    @FXML
+    public void onUpdate() {
+        int index = listView.getSelectionModel().getSelectedIndex();
+        loadFromFile();
+        lblMsg.setText("Persona actualizada con exito ");
+        lblMsg.setStyle("-fx-text-fill: green");
+        String name = txtName.getText();
+        String email = txtEmail.getText();
+        String edad = txtEdad.getText();
+        try{
+            service.updatePerson(index, name, email, edad);
+            txtName.clear();
+            txtEmail.clear();
+            txtEdad.clear();
+            loadFromFile();
+        }catch (IOException e){
+            lblMsg.setText("Hubo un error ");
+            lblMsg.setStyle("-fx-text-fill: red");
+
+        }catch (IllegalArgumentException ex){
+            lblMsg.setText(ex.getMessage());
+            lblMsg.setStyle("-fx-text-fill: red");
+        }
+    }
+
+    @FXML
+    public void onDelete(){
+        int index = listView.getSelectionModel().getSelectedIndex();
+        try{
+            service.deletePerson(index);
+            loadFromFile();
+            lblMsg.setText("persona eliminada correctamente");
+            lblMsg.setStyle("-fx-text-fill: green");
+            txtName.clear();
+            txtEmail.clear();
+            txtEdad.clear();
+        }catch (IOException e){
+            lblMsg.setText("hubo un error en el archivo");
+            lblMsg.setStyle("-fx-text-fill: red");
+        }
+    }
+
+    public void onReload(){
+        loadFromFile();
+    }
+
+    private void loadFromFile(){
+        try{
+            List<String> items = service.loadDataForList();
+            data.setAll(items);
+            lblMsg.setText("Datos cargados Exitosamente ");
+            lblMsg.setStyle("-fx-text-fill: green");
+        }catch (IOException e){
+            lblMsg.setText(e.getMessage());
+            lblMsg.setStyle("-fx-text-fill: red");
+        }
+    }
+    private void loadDataToForm(String item){
+        String[] parts = item.split("-");
+        txtName.setText(parts[0]);
+        txtEmail.setText(parts[1]);
+        txtEdad.setText(parts[2]);
     }
 }
